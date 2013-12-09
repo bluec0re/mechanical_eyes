88bb14116a1ed9e5bfc3bf952786443d472bdbcd
Timo Schmid <timo.schmid@stud.uni-heidelberg.de>
2013-12-08 18:53:32 -0700

    initial commit

---
+++ persontracker/src/tracker.h
@@ -0,0 +1,7 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+void setup();
+#ifdef __cplusplus
+}
+#endif
---
+++ persontracker/standalone/main.cpp
@@ -0,0 +1,6 @@
+#include <tracker.h>
+
+int main(int argc, char **argv) {
+    setup();
+    return 0;
+}
