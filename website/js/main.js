b4f6082a238925b28fec4700ab92eb8cdb3c9e6e
Timo Schmid <coding@bluec0re.eu>
2014-04-04 01:51:14 +0200

    added hover effect + non js functionality

--- website/js/main.js
+++ website/js/main.js
@@ -2,23 +2,24 @@ var switchPage = function(from, to) {
     if(from != "#landingpage")
 		$(from).fadeOut();
 	$(to).fadeIn();
-}
+};
 
 var makelinks = function(from, to) {
     $("#bullseye_" + to).click(function() {
         switchPage("#" + from, "#" + to);
+        return false;
     });
 
     $("#back_" + to).click(function() {
         switchPage("#" + to, "#" + from);
     });
-	
+
 	$(document).keyup(function(e) {
-		if (e.keyCode == 27) { 
+		if (e.keyCode == 27) {
 			switchPage("#" + to, "#" + from);
 		}
 	});
-}
+};
 
 makelinks("landingpage", "software");
 makelinks("landingpage", "hardware");
@@ -27,3 +28,5 @@ makelinks("landingpage", "probleme");
 makelinks("landingpage", "planung");
 makelinks("landingpage", "team");
 makelinks("landingpage", "downloads");
+
+$("section.subpage").addClass("tmp_hidden").addClass("subpage_js");
